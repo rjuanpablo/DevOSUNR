70ce4e56daab2f37ee5b62cacddba12d9c15c3b4
Juan Pablo <juanpabloruizbre@gmail.com>
2023-05-17 11:32:05 -0300

    Esto deberia bajar nelson para seguir

--- test1/src/App.jsx
+++ test1/src/App.jsx
@@ -1,3 +1,5 @@
+import Sidebar from  './components/Sidebar'
+
 function App() {
 
   return (
--- test1/src/components/Sidebar.jsx
+++ test1/src/components/Sidebar.jsx
@@ -1,38 +1,48 @@
-
-const links = [
-	{
-		id: 1,
-		text: 'inicio',
-		url: '/link1',
-	},
-	{
-		id: 2,
-		text: 'menu1',
-		url: '/link2',
-	},
-	{
-		id: 3,
-		text: 'menu2',
-		url: '/link3',
-	}
-]
+import { FaBeer, FaLink } from 'react-icons/fa';
 
 const Sidebar = () => {
 
+	const links = [
+		{
+			id: 1,
+			icono: <FaBeer />,
+			text: 'inicio',
+			url: '/link1',
+		},
+		{
+			id: 2,
+			icono: <FaBeer />,
+			text: 'menu1',
+			url: '/link2',
+		},
+		{
+			id: 3,
+			icono: <FaBeer />,
+			text: 'menu2',
+			url: '/link3',
+		}
+	]
 	
 	return ( 
-		<section className="w-2/5 bg-black">
-		<div className="flex flex-space-between">
-			<div>
-			{links.map((link) => {
-				<a key={link.id} href={link.url}>{link.text}</a>
-			})
-			}
-			</div>
-			<div>
-				cerrar sesion
-			</div>
-		</div>
+		<section className="w-12 h-screen bg-slate-300 sm:w-24">
+		    <div className="flex flex-col min-h-screen">
+      <div>
+        <h1><p class="block sm:hidden"><FaLink /></p><p class="hidden sm:block">Links</p></h1>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id} className="flex align-middle">
+              {link.icono}<a href={link.url} className="sm:"><p class="hidden sm:block">{link.text}</p></a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex-grow"></div>
+      <div className="flex justify-center pb-4">
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Cerrar sesión
+        </button>
+      </div>
+    </div>
 		</section>
 	);
 }
