87d3f7b07e1decacdc4a1eba482e79e6de890861
Nel Rodriguez <nel.rodriguez22@gmail.com>
2023-05-18 13:32:15 -0300

    menu y algunos estilos

--- test1/src/components/Container.jsx
+++ test1/src/components/Container.jsx
@@ -2,7 +2,7 @@
 const Container = ({isOpen, children}) => {
 
 	return ( 
-		<div className={`flex flex-col min-h-screen relative ${isOpen ? 'open' : 'hidden'}`}>
+		<div className={`flex flex-col gap-2 min-h-screen relative bg-slate-300 transition-all ${isOpen ? 'w-48' : 'w-12'}`}>
 			{children}
 		</div>
 	);
--- test1/src/datos/links.jsx
+++ test1/src/datos/links.jsx
@@ -3,19 +3,19 @@ import { FaBeer } from 'react-icons/fa';
 export const links = [
 	{
 		id: 1,
-		icono: <FaBeer />,
+		icono: <FaBeer size={20}/>,
 		text: 'inicio',
 		url: '/link1',
 	},
 	{
 		id: 2,
-		icono: <FaBeer />,
+		icono: <FaBeer size={20}/>,
 		text: 'menu1',
 		url: '/link2',
 	},
 	{
 		id: 3,
-		icono: <FaBeer />,
+		icono: <FaBeer size={20}/>,
 		text: 'menu2',
 		url: '/link3',
 	}
